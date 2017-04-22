902298372cc965c05842f1305eb3730f1129cfc1
Lars Lundqvist <lars_lundqvist@hotmail.com>
2017-04-22 17:45:17 +0200

    Card move animation when drawing and hovering cards

--- Assets/Scripts/HandScript.cs
+++ Assets/Scripts/HandScript.cs
@@ -18,8 +18,11 @@ public class HandScript : MonoBehaviour {
 	[Range(-1f, 1f)]
 	public float cardForwardOffset;
 
-	// Use this for initialization
-	void Start () {
+    [Range(0f,1f)]
+    public float movingSpeedOfCard;
+
+    // Use this for initialization
+    void Start () {
 		main = this;
 		cardStack = new List<GameObject>();
 	}
@@ -28,13 +31,17 @@ public class HandScript : MonoBehaviour {
 	void Update () {
 		for (int i = 0; i < cardStack.Count; i++)
 		{
+            Vector3 targetPosition;
             if (cardStack[i].GetComponent<HoverOver>().isHoveringOver)
             {
-                cardStack[i].transform.position = this.transform.position + this.transform.forward * cardForwardOffset * i + this.transform.right * cardSpacing * i + this.transform.up * hoverOffset;
+                targetPosition = this.transform.position + this.transform.forward * cardForwardOffset * i + this.transform.right * cardSpacing * i + this.transform.up * hoverOffset;
             }
             else {
-                cardStack[i].transform.position = this.transform.position + this.transform.forward * cardForwardOffset * i + this.transform.right * cardSpacing * i;
+                targetPosition = this.transform.position + this.transform.forward * cardForwardOffset * i + this.transform.right * cardSpacing * i;
             }
+
+            cardStack[i].transform.position = Vector3.Lerp(cardStack[i].transform.position, targetPosition, movingSpeedOfCard);
+
 			cardStack[i].transform.rotation = this.transform.rotation;
 			cardStack[i].transform.Rotate(cardStack[i].transform.up, cardRotation);
 		}
