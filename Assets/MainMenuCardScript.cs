d04af11d039561997c6146df2989dd41b88fe4bc
Pete <peteretep@live.dk>
2017-04-23 22:51:39 +0200

    Click on cards in main menu to make them jump

--- Assets/MainMenuCardScript.cs
+++ Assets/MainMenuCardScript.cs
@@ -2,30 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainMenuCardScript : MonoBehaviour {
+public class MainMenuCardScript : MonoBehaviour
+{
 	public Material[] materials;
 	public float lifeTime = 10;
 	private float timeAlive = 0;
 	public float maxRotationForce;
 	public float maxForceAdded;
+	public float clickForce;
+	Rigidbody r;
 
 	// Use this for initialization
-	void Start () {
+	void Start()
+	{
 		GetComponent<Renderer>().sharedMaterial = materials[Random.Range(0, materials.Length - 1)];
 
-		Rigidbody r = GetComponent<Rigidbody>();
+		r = GetComponent<Rigidbody>();
 
 		r.AddTorque(new Vector3(Random.Range(-maxRotationForce, maxRotationForce), Random.Range(-maxRotationForce, maxRotationForce), Random.Range(-maxRotationForce, maxRotationForce)));
 		r.AddForce(new Vector3(Random.Range(-maxForceAdded, maxForceAdded), Random.Range(-maxForceAdded, maxForceAdded), Random.Range(-maxForceAdded, maxForceAdded)));
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
 		timeAlive += Time.deltaTime;
 
 		if (timeAlive >= lifeTime)
 		{
 			Destroy(this.gameObject);
 		}
+	}
+
+	public void OnMouseDown()
+	{
+		r.AddForce(new Vector3(0, clickForce, 0));
 	}
 }
