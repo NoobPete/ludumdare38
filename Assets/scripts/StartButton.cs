cafd91c0ea9753a999e2a05a9c0b71d0f49486fc
Lars Lundqvist <lars_lundqvist@hotmail.com>
2017-04-23 19:47:26 +0200

    Made startmenu scene. Also started making logo

---
+++ Assets/scripts/StartButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ButtonClick()
+    {
+        SceneManager.LoadScene("main");
+    }
+}
