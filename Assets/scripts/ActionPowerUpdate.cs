69711c609ae0ef048e6fe88f02518f732b4dae23
Lars Lundqvist <lars_lundqvist@hotmail.com>
2017-04-23 15:46:01 +0200

    Added action symbol and text

---
+++ Assets/scripts/ActionPowerUpdate.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActionPowerUpdate : MonoBehaviour {
+    private Text textField;
+
+
+	// Use this for initialization
+	void Start () {
+        textField = this.GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        textField.text = "" + GameMasterScript.main.actionPointsLeft;
+	}
+}
