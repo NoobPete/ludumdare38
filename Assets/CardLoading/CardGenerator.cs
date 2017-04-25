7a2039e55bf7683b97f7cb7b3a96c4363ffb24f3
Pete <peteretep@live.dk>
2017-04-25 18:35:20 +0200

    Read and imports from csv file

---
+++ Assets/CardLoading/CardGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardGenerator : MonoBehaviour
+{
+	private static string[,] cardStats;
+	public TextAsset csvFile;
+	private static GameObject blankCardStatic;
+	public GameObject blankCard;
+
+	// Use this for initialization
+	void Awake()
+	{
+		cardStats = CSVReader.SplitCsvGrid(csvFile.text);
+		blankCardStatic = blankCard;
+	}
+
+	public static string GetItemInfo(int cardID, int column)
+	{
+		return cardStats[cardID, column];
+	}
+
+	public static GameObject GenerateNewCard(int cardID)
+	{
+		GameObject result = Instantiate(blankCardStatic);
+
+		Text[] textFields = result.GetComponentsInChildren<Text>();
+		textFields[0].text = cardStats[0, cardID];
+		textFields[1].text = cardStats[1, cardID];
+		textFields[2].text = cardStats[2, cardID];
+
+		return result;
+	}
+
+	public static int NumberOfCards()
+	{
+		return cardStats.GetLength(0);
+	}
+}
---
+++ Assets/CardLoading/CardShowRoomScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardShowRoomScript : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+		int numberOfCards = CardGenerator.NumberOfCards();
+
+		for (int i = 0; i < numberOfCards; i++)
+		{
+			GameObject o = CardGenerator.GenerateNewCard(i + 1);
+			o.transform.position = new Vector3(-1.2f * i, 0, 0);
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
